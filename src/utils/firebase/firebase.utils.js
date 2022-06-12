9c27a51537b49a819cff7b9b93c04a2ed9e5bce2
Diego Perlo <diego.perlo@quadminds.com>
2022-06-12 19:07:51 -0300

    key

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -21,12 +21,12 @@ import {
 } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDDU4V-_QV3M8GyhC9SVieRTDM4dbiT0Yk',
-  authDomain: 'crwn-clothing-db-98d4d.firebaseapp.com',
-  projectId: 'crwn-clothing-db-98d4d',
-  storageBucket: 'crwn-clothing-db-98d4d.appspot.com',
-  messagingSenderId: '626766232035',
-  appId: '1:626766232035:web:506621582dab103a4d08d6',
+  apiKey: "AIzaSyAC9PaJ-MnSTPZmgaUhXOGFyXizH6GzL18",
+  authDomain: "crwn-clothing-db-1fc6e.firebaseapp.com",
+  projectId: "crwn-clothing-db-1fc6e",
+  storageBucket: "crwn-clothing-db-1fc6e.appspot.com",
+  messagingSenderId: "39328455729",
+  appId: "1:39328455729:web:b737793f251eb91e6bc910",
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
